b695f981de8980381737e64a8f0e5bbb11675119
Max <das@ist.mail>
2013-11-12 14:22:06 +0100

    test

--- SQ_Java/src/gettingStarted/HelloWorld.java
+++ SQ_Java/src/gettingStarted/HelloWorld.java
@@ -3,7 +3,12 @@ package gettingStarted;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		char[] a = "Hello World".toCharArray();
+
+		for (int i= 0; i < a.length; i++) {
+			System.out.println("Give me a " + a[i] + "! ... " + a[i]);
+		}
+			
 	}
 
 }
